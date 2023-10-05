eb7e5bf235e6a645551eb2e52423f80e3d6d3b1f
ravilushqa <galaktionov.r@gmail.com>
2023-10-05 14:13:24 +0800

    Add Celsius to Fahrenheit temperature conversion handler
    Implemented a message handler for converting Celsius temperatures to Fahrenheit in main.go. This was necessary to facilitate communication between the temperature-celcius and temperature-fahrenheit topics in our router. Additionally, created a new subscriber for the temperature-fahrenheit topic and updated dependency files.

--- 04-router/02-no-publisher-handlers/main.go
+++ 04-router/02-no-publisher-handlers/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
-	"strconv"
 
 	"github.com/ThreeDotsLabs/watermill"
 	"github.com/ThreeDotsLabs/watermill-redisstream/pkg/redisstream"
@@ -30,27 +30,13 @@ func main() {
 		panic(err)
 	}
 
-	pub, err := redisstream.NewPublisher(redisstream.PublisherConfig{
-		Client: rdb,
-	}, logger)
-	if err != nil {
-		panic(err)
-	}
-
-	router.AddHandler(
+	router.AddNoPublisherHandler(
 		"celcius_to_fahrenheit",
-		"temperature-celcius",
-		sub,
 		"temperature-fahrenheit",
-		pub,
-		func(msg *message.Message) ([]*message.Message, error) {
-			t, err := celciusToFahrenheit(string(msg.Payload))
-			if err != nil {
-				return nil, err
-			}
-
-			newMsg := message.NewMessage(watermill.NewUUID(), []byte(t))
-			return []*message.Message{newMsg}, nil
+		sub,
+		func(msg *message.Message) error {
+			fmt.Println("Temperature read:", string(msg.Payload))
+			return nil
 		},
 	)
 
@@ -58,13 +44,4 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-}
-
-func celciusToFahrenheit(temperature string) (string, error) {
-	celcius, err := strconv.Atoi(temperature)
-	if err != nil {
-		return "", err
-	}
-
-	return strconv.Itoa(celcius*9/5 + 32), nil
 }
