b508336bbfc2dc77c93f30d279ccdce5448b93e5
ravilushqa <galaktionov.r@gmail.com>
2023-10-06 14:09:49 +0800

    Writing Mocks

---
+++ 08-component-testing/03-writing-mocks/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"time"
+)
+
+type IssueReceiptRequest struct {
+	TicketID string `json:"ticket_id"`
+	Price    Money  `json:"price"`
+}
+
+type Money struct {
+	Amount   string `json:"amount"`
+	Currency string `json:"currency"`
+}
+
+type IssueReceiptResponse struct {
+	ReceiptNumber string    `json:"number"`
+	IssuedAt      time.Time `json:"issued_at"`
+}
+
+type ReceiptsService interface {
+	IssueReceipt(ctx context.Context, request IssueReceiptRequest) (IssueReceiptResponse, error)
+}
+
+type ReceiptsServiceMock struct {
+	IssuedReceipts []IssueReceiptRequest
+}
+
+func (m *ReceiptsServiceMock) IssueReceipt(_ context.Context, request IssueReceiptRequest) (IssueReceiptResponse, error) {
+	m.IssuedReceipts = append(m.IssuedReceipts, request)
+	return IssueReceiptResponse{
+		ReceiptNumber: request.TicketID,
+		IssuedAt:      time.Now(),
+	}, nil
+}
