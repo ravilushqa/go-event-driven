722198656aca6316f18e4733884863c223ff0316
ravilushqa <galaktionov.r@gmail.com>
2023-10-10 14:20:09 +0800

    Implementing the Event Handler

---
+++ 09-cqrs-events/05-event-handler/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+
+	"github.com/ThreeDotsLabs/watermill/components/cqrs"
+)
+
+type FollowRequestSent struct {
+	From string `json:"from"`
+	To   string `json:"to"`
+}
+
+type EventsCounter interface {
+	CountEvent() error
+}
+
+func NewFollowRequestSentHandler(counter EventsCounter) cqrs.EventHandler {
+	return cqrs.NewEventHandler(
+		"NewFollowRequestSentHandler",
+		func(ctx context.Context, event *FollowRequestSent) error {
+			return counter.CountEvent()
+		},
+	)
+}
