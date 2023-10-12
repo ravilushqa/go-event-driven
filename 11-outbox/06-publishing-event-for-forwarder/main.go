f7bf14150f9fc0ba53f539b89eee4c9bdb925a14
ravilushqa <galaktionov.r@gmail.com>
2023-10-12 17:41:27 +0800

    Publishing events within transaction with forwarder

---
+++ 11-outbox/06-publishing-event-for-forwarder/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+
+	"github.com/ThreeDotsLabs/watermill"
+	watermillSQL "github.com/ThreeDotsLabs/watermill-sql/v2/pkg/sql"
+	"github.com/ThreeDotsLabs/watermill/components/forwarder"
+	"github.com/ThreeDotsLabs/watermill/message"
+	_ "github.com/lib/pq"
+)
+
+var outboxTopic = "events_to_forward"
+
+func PublishInTx(
+	msg *message.Message,
+	tx *sql.Tx,
+	logger watermill.LoggerAdapter,
+) error {
+	sqlPublisher, err := watermillSQL.NewPublisher(
+		tx,
+		watermillSQL.PublisherConfig{
+			SchemaAdapter: watermillSQL.DefaultPostgreSQLSchema{},
+		},
+		logger,
+	)
+	if err != nil {
+		return err
+	}
+
+	publisher := forwarder.NewPublisher(sqlPublisher, forwarder.PublisherConfig{
+		ForwarderTopic: outboxTopic,
+	})
+
+	return publisher.Publish("ItemAddedToCart", msg)
+}
