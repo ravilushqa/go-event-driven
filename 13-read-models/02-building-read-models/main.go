349074ac7359dcbf6510d5af8a1b36cffc66cfa7
ravilushqa <galaktionov.r@gmail.com>
2023-10-19 11:42:48 +0800

    Read Models

---
+++ 13-read-models/02-building-read-models/main.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill/components/cqrs"
+	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/shopspring/decimal"
+)
+
+type InvoiceIssued struct {
+	InvoiceID    string
+	CustomerName string
+	Amount       decimal.Decimal
+	IssuedAt     time.Time
+}
+
+type InvoicePaymentReceived struct {
+	PaymentID  string
+	InvoiceID  string
+	PaidAmount decimal.Decimal
+	PaidAt     time.Time
+
+	FullyPaid bool
+}
+
+type InvoiceVoided struct {
+	InvoiceID string
+	VoidedAt  time.Time
+}
+
+type InvoiceReadModel struct {
+	InvoiceID    string
+	CustomerName string
+	Amount       decimal.Decimal
+	IssuedAt     time.Time
+
+	FullyPaid     bool
+	PaidAmount    decimal.Decimal
+	LastPaymentAt time.Time
+
+	Voided   bool
+	VoidedAt time.Time
+}
+
+type InvoiceReadModelStorage struct {
+	invoices map[string]InvoiceReadModel
+	payments map[string]InvoicePaymentReceived
+}
+
+func NewInvoiceReadModelStorage() *InvoiceReadModelStorage {
+	return &InvoiceReadModelStorage{
+		invoices: make(map[string]InvoiceReadModel),
+		payments: make(map[string]InvoicePaymentReceived),
+	}
+}
+
+func (s *InvoiceReadModelStorage) Invoices() []InvoiceReadModel {
+	invoices := make([]InvoiceReadModel, 0, len(s.invoices))
+	for _, invoice := range s.invoices {
+		invoices = append(invoices, invoice)
+	}
+	return invoices
+}
+
+func (s *InvoiceReadModelStorage) InvoiceByID(id string) (InvoiceReadModel, bool) {
+	invoice, ok := s.invoices[id]
+	return invoice, ok
+}
+
+func (s *InvoiceReadModelStorage) OnInvoiceIssued(ctx context.Context, event *InvoiceIssued) error {
+	invoice, exists := s.InvoiceByID(event.InvoiceID)
+	if exists {
+		return fmt.Errorf("invoice %s already exists", invoice.InvoiceID)
+	}
+
+	s.invoices[event.InvoiceID] = InvoiceReadModel{
+		InvoiceID:    event.InvoiceID,
+		CustomerName: event.CustomerName,
+		Amount:       event.Amount,
+		IssuedAt:     event.IssuedAt,
+	}
+
+	return nil
+}
+
+func (s *InvoiceReadModelStorage) OnInvoicePaymentReceived(ctx context.Context, event *InvoicePaymentReceived) error {
+	// could be race condition
+	if _, exists := s.payments[event.PaymentID]; exists {
+		return nil
+	}
+	s.payments[event.PaymentID] = *event
+
+	invoice, exists := s.InvoiceByID(event.InvoiceID)
+	if !exists {
+		return fmt.Errorf("invoice %s does not exist", event.InvoiceID)
+	}
+
+	invoice.PaidAmount = invoice.PaidAmount.Add(event.PaidAmount)
+	invoice.LastPaymentAt = event.PaidAt
+	invoice.FullyPaid = event.FullyPaid
+
+	s.invoices[event.InvoiceID] = invoice
+
+	return nil
+}
+
+func (s *InvoiceReadModelStorage) OnInvoiceVoided(ctx context.Context, event *InvoiceVoided) error {
+	invoice, exists := s.InvoiceByID(event.InvoiceID)
+	if !exists {
+		return fmt.Errorf("invoice %s does not exist", event.InvoiceID)
+	}
+
+	invoice.Voided = true
+	invoice.VoidedAt = event.VoidedAt
+
+	s.invoices[event.InvoiceID] = invoice
+
+	return nil
+}
+
+func NewRouter(storage *InvoiceReadModelStorage, eventProcessorConfig cqrs.EventProcessorConfig, watermillLogger watermill.LoggerAdapter) (*message.Router, error) {
+	router, err := message.NewRouter(message.RouterConfig{}, watermillLogger)
+	if err != nil {
+		return nil, fmt.Errorf("could not create router: %w", err)
+	}
+
+	eventProcessor, err := cqrs.NewEventProcessorWithConfig(router, eventProcessorConfig)
+	if err != nil {
+		return nil, fmt.Errorf("could not create command processor: %w", err)
+	}
+
+	err = eventProcessor.AddHandlers(
+		cqrs.NewEventHandler(
+			"InvoiceIssued",
+			func(ctx context.Context, event *InvoiceIssued) error {
+				return storage.OnInvoiceIssued(ctx, event)
+			},
+		),
+		cqrs.NewEventHandler(
+			"InvoicePaymentReceived",
+			func(ctx context.Context, event *InvoicePaymentReceived) error {
+				return storage.OnInvoicePaymentReceived(ctx, event)
+			},
+		),
+		cqrs.NewEventHandler(
+			"InvoiceVoided",
+			func(ctx context.Context, event *InvoiceVoided) error {
+				return storage.OnInvoiceVoided(ctx, event)
+
+			},
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("could not add event handlers: %w", err)
+	}
+
+	return router, nil
+}
